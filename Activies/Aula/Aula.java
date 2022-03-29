b91833d7f3bf68efaea0136e843031fdb34e7451
Leonardo Faustino <leonardofaustino0@gmail.com>
2022-03-29 19:09:07 -0300

    em processo

---
+++ Activies/Aula/Aula.java
@@ -0,0 +1,30 @@
+package Aula;
+
+import java.util.Scanner;
+/////////////////////////////////// CALCULANDO RAIZ ///////////////////////////////////////////////
+ 
+class raiz {
+ 
+   public static void main( String[] args ) {
+      int a, b = 0;
+      var s = new Scanner( System.in);
+      System.out.println( "digite um numero: ");
+      a = s.nextInt();
+      b = (int) Math.sqrt(a);
+      System.out.println( "a raiz quadrada e: " + b );
+   }
+ 
+}
+////////////////////////////////// RAIZ C/ LOG, EXP, REALINT ////////////////////////////////////////////
+
+class raiz {
+ 
+    public static void main( String[] args ) {
+       int a, b = 0;
+       var s = new Scanner( System.in);
+       System.out.println( "digite um numero: ");
+       a = s.nextInt();
+       b = (int) Math.sqrt(a);
+       System.out.println( "a raiz quadrada e: " + b );
+    }
+  
