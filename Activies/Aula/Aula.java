4a085c736eba7b1f3716ddd1674f614655b398f3
Leonardo Faustino <leonardofaustino0@gmail.com>
2022-03-31 09:08:00 -0300

    primeita parte da aula

--- Activies/Aula/Aula.java
+++ Activies/Aula/Aula.java
@@ -1,30 +1,35 @@
 package Aula;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
-/////////////////////////////////// CALCULANDO RAIZ ///////////////////////////////////////////////
- 
-class raiz {
- 
-   public static void main( String[] args ) {
-      int a, b = 0;
-      var s = new Scanner( System.in);
-      System.out.println( "digite um numero: ");
-      a = s.nextInt();
-      b = (int) Math.sqrt(a);
-      System.out.println( "a raiz quadrada e: " + b );
-   }
- 
-}
-////////////////////////////////// RAIZ C/ LOG, EXP, REALINT ////////////////////////////////////////////
+public class Aula{
+   public static void main(String[] args) {
 
-class raiz {
- 
-    public static void main( String[] args ) {
-       int a, b = 0;
-       var s = new Scanner( System.in);
-       System.out.println( "digite um numero: ");
-       a = s.nextInt();
-       b = (int) Math.sqrt(a);
-       System.out.println( "a raiz quadrada e: " + b );
-    }
-  
+      Scanner scan = new Scanner(System.in);      //←
+      double a, b;                                //← funçao para ler e definir numeros
+      System.out.println("digite um numero: ");   //←
+      a = scan.nextDouble();                      //←
+
+///////////////////////////////////////////// RAIZ /////////////////////////////////////////
+      b = Math.sqrt(a);
+
+      System.out.println("a raiz de " + a + " e: " + b);
+
+///////////////////////////////////// RAIZ, EXP, LOG, c/ REALINT ////////////////////////////////
+      b = (Math.exp(1/2 * Math.log(a)));
+
+      DecimalFormat formatador = new DecimalFormat(".");
+
+      System.out.println("o valor int de exp, log de "+ a + " e: "+ formatador.format(b));
+
+//////////////////////////////////// RAIZ, EXP, LOG, S/ REALINT ///////////////////////////////
+   
+      b = (Math.exp(1/2 * Math.log(a)));
+
+      System.out.println("o valor de exp, log "+ a + " e: "+ b);
+
+//////////////////////////////////
+
+
+   }
+}
